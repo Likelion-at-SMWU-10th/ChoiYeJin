d174be8cbaaa6065e06fc82b524bedf6563e5494
OaYenn <amy350500@gmail.com>
2022-05-02 14:16:29 +0900

    API key 삭제

--- weather.py
+++ weather.py
@@ -2,7 +2,6 @@ import requests
 import json
 
 city = "Seoul"
-apikey = "########"
 lang = "kr"
 
 api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&lang={lang}&units=metric"
